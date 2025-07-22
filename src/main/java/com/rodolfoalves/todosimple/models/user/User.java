ddd6c578fa8039c61520b743691fa0a675adc42f
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-22 16:42:01 -0300

    feat(user): add user attributes

---
+++ src/main/java/com/rodolfoalves/todosimple/models/user/User.java
@@ -0,0 +1,20 @@
+package com.rodolfoalves.todosimple.models.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 60, nullable = false)
+    private String password;
+}
