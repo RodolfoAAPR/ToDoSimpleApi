b0b39d0b4559b22505256159e546be68753cd740
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-23 16:51:53 -0300

    feat(user): add user repository interface

---
+++ src/main/java/com/rodolfoalves/todosimple/repositories/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.rodolfoalves.todosimple.repositories.user;
+
+import com.rodolfoalves.todosimple.models.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
