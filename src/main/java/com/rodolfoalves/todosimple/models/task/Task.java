3376931ea716f42bd3ea9f6363e7ce1d73d6af3b
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-28 17:16:07 -0300

    refactor(task): add json property

--- src/main/java/com/rodolfoalves/todosimple/models/task/Task.java
+++ src/main/java/com/rodolfoalves/todosimple/models/task/Task.java
@@ -1,11 +1,15 @@
 package com.rodolfoalves.todosimple.models.task;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.rodolfoalves.todosimple.models.user.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -33,4 +37,7 @@ public class Task {
     @Size(min = 5, max = 255)
     private String description;
 
+    @OneToMany
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<Task> tasks = new ArrayList<Task>();
 }
