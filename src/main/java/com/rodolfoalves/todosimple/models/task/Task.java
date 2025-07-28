da21e3105ae8a2745d9264d4b6a529b22f46a08b
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-28 17:10:01 -0300

    refactor(task): add all args constructor, setters and getters with lombok

--- src/main/java/com/rodolfoalves/todosimple/models/task/Task.java
+++ src/main/java/com/rodolfoalves/todosimple/models/task/Task.java
@@ -5,11 +5,18 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.Objects;
 
 @Entity
 @Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Task {
     public static final String TABLE_NAME = "task";
 
@@ -28,64 +35,4 @@ public class Task {
     @Size(min = 5, max = 255)
     private String description;
 
-    public Task(){}
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return false;
-        if (obj == null)
-            return false;
-        if (!(obj instanceof Task))
-            return false;
-
-        Task other = (Task) obj;
-        if (this.id == null) {
-            if (other.id != null)
-                return false;
-            else if (!this.id.equals(other.id))
-                return false;
-        }
-
-        return Objects.equals(id, other.id) && Objects.equals(user, other.user)
-                && Objects.equals(description, other.description);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (this.id == null ? 0 : this.id.hashCode());
-        return result;
-    }
-
-    public Task(Long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
