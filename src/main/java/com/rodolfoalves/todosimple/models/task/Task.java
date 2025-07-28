0c233d9945b473e9678f412858e28c4086ac8072
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-28 17:12:30 -0300

    refactor(task): add all args constructor, setters and getters with lombok

--- src/main/java/com/rodolfoalves/todosimple/models/task/Task.java
+++ src/main/java/com/rodolfoalves/todosimple/models/task/Task.java
@@ -5,10 +5,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 @Entity
@@ -17,6 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Task {
     public static final String TABLE_NAME = "task";
 
