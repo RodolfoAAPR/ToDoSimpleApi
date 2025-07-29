8c2a542b3b6224de728a0363c2fbccc63f6cb58b
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-07-29 17:54:56 -0300

    feat(GlobalExceptionHandler): add method build error response and handle data integrity violation

--- src/main/java/com/rodolfoalves/todosimple/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/rodolfoalves/todosimple/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.rodolfoalves.todosimple.exceptions;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -37,5 +40,38 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
         }
         return ResponseEntity.unprocessableEntity().body(errorResponse);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(
+            Exception exception,
+            HttpStatus httpStatus,
+            WebRequest request){
+        return buildErrorResponse(exception, exception.getMessage(), httpStatus, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(
+            Exception exception,
+            String message,
+            HttpStatus httpStatus,
+            WebRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+        if (this.printStackTrace) {
+            errorResponse.setStackTrace(ExceptionUtils.getStackTrace(exception));
+        }
+        return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(
+            DataIntegrityViolationException dataIntegrityViolationException,
+            WebRequest request) {
+        String errorMessage = dataIntegrityViolationException.getMostSpecificCause().getMessage();
+        log.error("Failed to save entity integrity problems" + errorMessage, dataIntegrityViolationException);
+        return buildErrorResponse(
+          dataIntegrityViolationException,
+          errorMessage,
+          HttpStatus.CONFLICT,
+          request);
     }
 }
